006d38503a7a8953d40279d262720395d70f6da7
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2015-02-11 21:32:13 +0200

    filehandler and raw done

--- caen-raw.cc
+++ caen-raw.cc
@@ -1,61 +1,170 @@
 #include"caen-raw.h"
-#include<string.h>
+#include<string>
 #include<iostream>
+#include<iomanip>
 #include<stdio.h>
 
-using namespace std;
- //File /*{{{*/
- File::File(std::string& ifn){
- fRawFileP=NULL;
- fRawFileP=fopen(ifn.c_str(),"r");
- if(fRawFileP==NULL){
- cerr<<"File not found "<<ifn<<endl;
- }else{
- cerr<<"File "<<ifn<<" successfuly oppened"<<endl;
- }
- }
- File::~File(){
- cout<<fRawFileP<<endl;
- fclose(fRawFileP);
- }
- template<typename T> bool File::GetFileData(T& dat){
- int bytesRead=fread((void*)&dat,sizeof(T),1,fRawFileP);
- if(bytesRead==sizeof(T)) return true;
- cerr<<"EOF"<<endl;
- return false;
- }
- template<typename T> bool File::GetFileDataArray(T* dat){
- int bytesRead=fread((void*)dat,sizeof(T)*sizeof(dat),1,fRawFileP);
- cout<<"sizof(t) "<<sizeof(T)<< " sizeof(dat)" <<sizeof(dat)<<endl;
- if(bytesRead==sizeof(T)) return true;
- cerr<<"EOF"<<endl;
- return false;
- }
- /*}}}*/
+
+//File /*{{{*/
+FileHandler::FileHandler(std::string& ifn){
+  fRawFileP=NULL;
+  fRawFileP=fopen(ifn.c_str(),"r");
+  if(fRawFileP==NULL){
+    std::cerr<<"File not found "<<ifn<<std::endl;
+  }else{
+    std::cerr<<"File "<<ifn<<" successfuly oppened"<<std::endl;
+  }
+}
+FileHandler::~FileHandler(){
+  std::cout<<fRawFileP<<std::endl;
+  fclose(fRawFileP);
+}
+template<typename T> bool FileHandler::GetFileData(T& dat){
+  int bytesRead=fread((void*)&dat,sizeof(T),1,fRawFileP);
+  if(bytesRead==sizeof(T)) return true;
+  std::cerr<<"EOF"<<std::endl;
+  return false;
+}
+template<typename T> bool FileHandler::GetFileDataArray(T* dat,unsigned int n){
+  int bytesRead=fread((void*)dat,sizeof(T),n,fRawFileP);
+  std::cout<<"sizof(t) "<<sizeof(T)<< " sizeof(dat)" <<sizeof(dat)<<"  bytes "<<bytesRead<<std::endl;
+  if(bytesRead==sizeof(T)*n) return true;
+  std::cerr<<"EOF"<<std::endl;
+  return false;
+}
+void FileHandler::Reset(){
+  fseek(fRawFileP,0,SEEK_SET);
+  return;
+}
+/*}}}*/
 
 
 
-//EventHeader::EventHeader(FileHandler* fh){
-//  fRawFile=fh;
-//  PullRawData();
-//}
-//
-//EventHeader::~EventHeader(){
-//}
-//
-//
-//
-//
-//Event::Event(FileHandler* fh){
-//  fRawFile=fh;
-//  fEventHeader=new EventHeader(fh);
-//  fEventHeader->GetEventSize();
-//}
-//
-//Event::~Event(){
-//  delete fEventHeader;
-//}
-//
-//
-//
-//
+EventHeader::EventHeader(FileHandler* fh){
+  fRawFile=fh;
+  PullRawData();
+}
+
+EventHeader::~EventHeader(){
+}
+bool EventHeader::PullRawData(){
+  fRawFile->GetFileData(fHeader);
+  return true;
+}
+
+void EventHeader::Info(){/*{{{*/
+
+  std::cout<<std::endl
+    <<"eventSize      " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.eventSize       <<std::endl
+    <<"header1010     " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.header1010      <<std::endl
+    <<"channelMask    " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.channelMask   <<"  "<<GetNChannels()<<"  bits set"<<std::endl
+    <<"reserved1      " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.reserved1       <<std::endl
+    <<"zero           " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.zero            <<std::endl
+    <<"reserved2      " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.reserved2       <<std::endl
+    <<"eventCounter   " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.eventCounter    <<std::endl
+    <<"reserved3      " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.reserved3       <<std::endl
+    <<"triggerTimeTag " << std::setfill('0') << std::setw(8)<<std::hex<<fHeader.triggerTimeTag  <<std::endl
+    <<std::endl;
+  return;
+}/*}}}*/
+
+uint32_t EventHeader::GetEventSize() const {
+  return fHeader.eventSize-4;
+  }
+
+unsigned int EventHeader::GetNChannels(){
+  unsigned int ret=0;
+  for(int i=0;i<8;i++){
+    ret+=fHeader.channelMask>>i&0x1;
+  }
+  return ret;
+}
+
+uint32_t EventHeader::GetRawChannelSize(){
+  uint32_t evtSize=GetEventSize();
+  unsigned int nChans=GetNChannels();
+  unsigned int ret=evtSize/nChans;
+  if(evtSize%nChans!=0){
+    std::cerr<<"warning GetRawChannelSize"<<std::endl;
+  }
+  return ret;
+}
+
+
+
+
+Event::Event(FileHandler* fh){
+  fRawFile=fh;
+  fEventHeader=new EventHeader(fh);
+  fEventHeader->Info();
+  fEventSize=fEventHeader->GetEventSize();
+  fChannelSize=fEventHeader->GetRawChannelSize();
+  fChannelsPerEvent=fEventHeader->GetNChannels();
+
+  fEventAllRaw=new raw_word_t[fEventSize];
+  fRawFile->Reset();// go to the beginning of the file
+
+
+}
+
+Event::~Event(){
+  delete fEventHeader;
+  delete[] fEventAllRaw;
+}
+
+bool Event::PullRawData(){
+  fEventHeader->PullRawData();
+  fRawFile->GetFileDataArray(fEventAllRaw,fEventSize);
+}
+
+
+void Event::Info(){
+  fEventHeader->Info();
+
+  for(int ch=0;ch<fChannelsPerEvent;++ch){
+    for(int word=0;word<fChannelSize;++word){
+      raw_word_t& rawWord=fEventAllRaw[ch*word];
+      if(rawWord.nSamples==3){
+        std::cout<<" chan "<<ch<<"   word "<<word<<" fields "
+          <<rawWord.sample1<<" "
+          <<rawWord.sample2<<" "
+          <<rawWord.sample3
+          <<std::endl;
+      }else if(rawWord.nSamples==2){
+        std::cout<<" chan "<<ch<<"   word "<<word<<" fields "
+          <<rawWord.sample1<<" "
+          <<rawWord.sample2
+          <<std::endl;
+      }else if(rawWord.nSamples==1){
+        std::cout<<" chan "<<ch<<"   word "<<word<<" fields "
+          <<rawWord.sample1
+          <<std::endl;
+      }else std::cerr<<"dummy word"<<std::endl;
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+  std::string fn="/home/daq/links/daq-optic-data/rawdata_2015_02_05_10_17_50";
+  FileHandler rawFile(fn);
+  Event evt(rawFile.GetInstance());
+  evt.PullRawData();
+  evt.Info();
+  evt.PullRawData();
+  evt.Info();
+
+  return 0;
+}
--- caen-raw.h
+++ caen-raw.h
@@ -1,60 +1,83 @@
 #ifndef _caen_raw_h_
 #define _caen_raw_h_
 
-#include<string.h>
+#include<string>
 #include<stdint.h>
 #include<stdio.h>
 
-  class File{/*{{{*/
+
+class FileHandler{
   public:
-  File(std::string& ifn);
-  ~File();
-  template<typename T> bool GetFileData(T& dat);
-  template<typename T> bool GetFileDataArray(T* dat);
+    FileHandler(std::string&);
+    ~FileHandler();
+    FileHandler* GetInstance(){return this;}
+    FileHandler(FileHandler&){;}
+    template<typename T> bool GetFileData(T& dat);
+    template<typename T> bool GetFileDataArray(T* dat, unsigned int n=1);
+    void Reset();
   private:
-  FILE *fRawFileP;
-  };/*}}}*/
+    FILE* fRawFileP;
+};
+
+class EventHeader{
+  public:
+    typedef struct{/*{{{*/
+      uint32_t eventSize   :28;
+      uint32_t header1010  : 4;
+      union __attribute__((packed)){
+        struct __attribute__((packed)){
+          unsigned int ch0:1;
+          unsigned int ch1:1;
+          unsigned int ch2:1;
+          unsigned int ch3:1;
+          unsigned int ch4:1;
+          unsigned int ch5:1;
+          unsigned int ch6:1;
+          unsigned int ch7:1;
+        };
+        unsigned int channelMask:8 ;
+      };
+      uint32_t reserved1   :16;
+      uint32_t zero        : 1;
+      uint32_t reserved2   : 7;
+      uint32_t eventCounter:24;
+      uint32_t reserved3   : 8;
+      uint32_t triggerTimeTag;
+    } __attribute__((packed)) header_t;/*}}}*/
+    EventHeader(FileHandler* fh);
+    ~EventHeader();
+    uint32_t GetEventSize() const ;
+    bool PullRawData();
+    void Info();
+    unsigned int GetNChannels();
+    uint32_t GetRawChannelSize();
+  private:
+    FileHandler* fRawFile;
+    header_t fHeader;
+};
 
 
-//class EventHeader{
-//  typedef struct{
-//    uint32_t eventSize   :28;
-//    uint32_t header1010  : 4;
-//    union {
-//      char ch[8]:1;
-//    }channelMask;
-//    uint32_t reserved1   :16;
-//    uint32_t zero        : 1;
-//    uint32_t reserved2   : 7;
-//    uint32_t eventCounter:24;
-//    uint32_t reserved3   : 8;
-//    uint32_t triggerTimeTag;
-//  }header_t;
-//
-//  public:
-//  EventHeader(FileHandler* fh);
-//  ~EventHeader();
-//  uint32_t GetEventSize() const {return fHeader.eventSize;}
-//  bool PullRawData();
-//  void Info();
-//
-//  private:
-//  FileHandler* fRawFile;
-//  header_t fHeader;
-//};
-//
-//
-//class Event{
-//  public:
-//    Event(FileHandler* fh);
-//    ~Event();
-//    bool PullRawData();
-//    void Info();
-//  private:
-//    FileHandler* fRawFile;
-//    EventHeader* fEventHeader;
-//
-//};
+class Event{
+  public:
+  typedef struct {
+    unsigned int sample1 :10;
+    unsigned int sample2 :10;
+    unsigned int sample3 :10;
+    unsigned int nSamples:2;
+  } __attribute__((packed)) raw_word_t;
+    Event(FileHandler* fh);
+    ~Event();
+    bool PullRawData();
+    void Info();
+  private:
+    FileHandler* fRawFile;
+    EventHeader* fEventHeader;
+    raw_word_t*  fEventAllRaw;
+    unsigned int fChannelsPerEvent;
+    uint32_t     fChannelSize;  // in uint32_t words
+    uint32_t     fEventSize;  // in uint32_t words
+
+};
 
 
 #endif
